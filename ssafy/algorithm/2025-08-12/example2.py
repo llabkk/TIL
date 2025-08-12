981858db5bb1d62f93bff0974f7f4d2c20ad072c
seungcheol_kim <kscheol25@gmail.com>
2025-08-12 10:37:20 +0900

    2025-08-12

--- ssafy/algorithm/2025-08-12/example2.py
+++ ssafy/algorithm/2025-08-12/example2.py
@@ -1,14 +1,13 @@
-import sys
-sys.stdin = open("input.txt", "r")
-
-def my_push(stack, top, a):
+def my_push(stack, a):
+    global top
     if top < len(stack)-1:
         top += 1
         stack[top] = a
     else:
         print("overflow!")
 
-def my_pop(stack, top):
+def my_pop(stack):
+    global top
     if top > -1:
         a = stack[top]
         top -= 1
@@ -17,24 +16,34 @@ def my_pop(stack, top):
         print("underflow!")
 
 def censor(string):
+    global top
     length = len(string)
-    top = -1
     stack = [0]*length
 
     reverse_string = string[::-1]
 
     for a in reverse_string:
         if a == ")":
-            my_push(stack, top, ")")
+            my_push(stack, ")")
         elif a == "(":
             if stack[top] == ")":
-                my_pop(stack, top)
+                my_pop(stack)
             else:
-                print("error!")
-                break
+                return False
+    if top == -1:
+        return True
+    else:
+        return False
 
 T = int(input())
 
 for tc in range(1, 1 + T):
+    top = -1
     string = input()
-    print(string)
+
+    result = censor(string)
+
+    if result:
+        print(f"#{tc} good!")
+    else:
+        print(f"#{tc} error")
